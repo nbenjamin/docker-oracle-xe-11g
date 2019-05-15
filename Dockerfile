31467a8c220d79a62eadd5d731159b3b1410af8d
nidhin <nidhinbenjamin@gmail.com>
2019-05-15 15:38:28 -0400

    add correspondence sql

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM ubuntu:18.04
 
-MAINTAINER Wei-Ming Wu <wnameless@gmail.com>
+MAINTAINER nbenjamine <test@gmail.com>
 
 ADD assets /assets
 RUN /assets/setup.sh
--- assets/setup.sh
+++ assets/setup.sh
@@ -44,6 +44,7 @@ echo "ALTER PROFILE DEFAULT LIMIT PASSWORD_VERIFY_FUNCTION NULL;" | sqlplus -s S
 echo "alter profile DEFAULT limit password_life_time UNLIMITED;" | sqlplus -s SYSTEM/oracle
 echo "alter user SYSTEM identified by oracle account unlock;" | sqlplus -s SYSTEM/oracle
 cat /assets/apex-default-pwd.sql | sqlplus -s SYSTEM/oracle
+cat /assets/correspondence.sql | sqlplus -s SYSTEM/oracle
 
 # Remove installation files
 rm -r /assets/
